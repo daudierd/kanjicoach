b21e31d6364a14c944ebc861f264700267732f3c
daudierd <daudierd@users.noreply.github.com>
2017-09-20 23:34:40 +0200

    fetch comboBox data & implement signal connectors

--- kanjicoach/configure.py
+++ kanjicoach/configure.py
@@ -18,7 +18,7 @@ class ConfigureDialog(QDialog):
         """Initialize the UI"""
         self.setWindowTitle("Configure Kanji Coach")
 
-        # TO DO: Uncomment last instruction line when tooltips are implemented 
+        #TODO Uncomment last instruction line when tooltips are implemented
         instructions = QLabel("<h4>Instructions:</h4>" +
             "<p>This window allows you to configure and customize Kanji Coach "+
             "add-on according to your learning style and objectives." +
@@ -31,6 +31,9 @@ class ConfigureDialog(QDialog):
         # First line allows the user to specify the deck used by the add-on
         grid.addWidget(QLabel("Study deck"), 1, 0)
         self.deck = QComboBox()
+        self.deck.addItems(self.getDecks())
+        self.deck.currentIndexChanged.connect(
+            lambda: self.updateFields(self.deck.currentText()))
         grid.addWidget(self.deck, 1, 1)
         # Second line allows the user to specify which field in the deck
         # contains the kanji
@@ -40,6 +43,7 @@ class ConfigureDialog(QDialog):
         # Third line allows the user to choose which stroke diagrams to display
         grid.addWidget(QLabel("Stroke diagrams"), 3, 0)
         self.strokes = QComboBox()
+        self.strokes.addItems(["Animated", "Kanjax diagrams"])
         grid.addWidget(self.strokes, 3, 1)
         # In case the user wants to use a custom field, a combo box will be
         # displayed
@@ -67,7 +71,9 @@ class ConfigureDialog(QDialog):
         self.suspend_unlearnt = QCheckBox("Suspend unlearnt cards")
 
         btn_ok = QPushButton("OK")
+        btn_ok.clicked.connect(self.onConfirm)
         btn_cancel = QPushButton("Cancel")
+        btn_cancel.clicked.connect(self.onCancel)
         hbox = QHBoxLayout()
         hbox.addStretch(1)
         hbox.addWidget(btn_ok)
@@ -84,6 +90,24 @@ class ConfigureDialog(QDialog):
 
         self.setLayout(vbox)
         self.resize(480, 550)
+
+    def getDecks(self):
+        #TODO Exclude empty decks
+        decks = mw.col.decks.allNames()
+        return sorted(decks)
+
+    def updateFields(self, deckName):
+        did = mw.col.decks.id(deckName)
+        cids = mw.col.decks.cids(did)
+        if cids:
+            # We assume that alls cards share the same model
+            card0 = mw.col.getCard(cids[0])
+            model = mw.col.models.get(card0.note().mid)
+            fields = mw.col.models.fieldNames(model)
+            self.kanji_fld.addItems(fields)
+        else:
+            QMessageBox.about(self, "Warning",
+                "Your deck does not contain any card!")
 
     def onCancel(self, mode):
         self.close()
