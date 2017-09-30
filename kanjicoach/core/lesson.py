b78f4d13ee83ff70e9bb277bb9cad6d9ac360de4
daudierd <daudierd@users.noreply.github.com>
2017-09-30 22:48:31 +0200

    define Lesson object & update specs

--- kanjicoach/core/lesson.py
+++ kanjicoach/core/lesson.py
@@ -5,7 +5,22 @@
 #
 # This module implements basic core functions used to generate lessons.
 
-def generate_lesson_kanji(config):
-    """Returns a list of kanji for the next lesson using the existing
-    configuration."""
-    pass
+import os
+from aqt import *
+
+class Lesson():
+    def __init__(self, config):
+        """Initializes the lesson with a Configuration object."""
+        # Can't initialize lesson without configuration
+        self.config = config
+
+    def generate_lesson(self):
+        """Returns a list of (kanji, meaning, parts, stroke_diagram) tuples for
+        the next lesson using the existing configuration and stores the
+        corresponding notes ids"""
+        pass
+
+    def submit_lesson(self):
+        """Reschedules notes for kanji returned on the last call of
+        generate_lesson, using the notes ids stored."""
+        pass
